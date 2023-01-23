a6e50ec8d64d76dee19f1746e85d01016b50c699
JozefJojo <97039061+JozefJojo@users.noreply.github.com>
2023-01-23 21:53:28 +0100

    start_Springboot

---
+++ BackEnd/My_Eshop/src/main/java/com/example/my_eshop/MyEshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEshopApplication.class, args);
+	}
+
+}
---
+++ BackEnd/My_Eshop/src/test/java/com/example/my_eshop/MyEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
