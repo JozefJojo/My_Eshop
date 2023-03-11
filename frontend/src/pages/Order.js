69b9fbe2cc8d5ae093d698278faa8e163a9cec2c
JozefJojo <97039061+JozefJojo@users.noreply.github.com>
2023-03-11 18:00:24 +0100

    FE-Order - added UserForm to OrderPage

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -4,13 +4,38 @@ import { ShopContext } from '../context/shop-context';
 import { OrderItem } from './cart/order-item';  
 import './cart/order.css';
 
-// import "./cart/cart.css";
-// import Button from 'react-bootstrap/Button';
-
 const Order = () =>{
 
     const [products, setProducts] = useState([])
-    const {cartItems} = useContext(ShopContext);
+    const {cartItems,getTotalCartPrice} = useContext(ShopContext);
+    const totalPrice = getTotalCartPrice();
+
+    const [user, setUser] = useState({
+        // id: "",
+        name: "",
+        email: "",
+        city: "",
+        address: "",
+      })
+
+      const setInputField = event => {
+        const { name, value } = event.target;
+        setUser((prevState) => ({
+          ...prevState,
+          [name]: value
+        }))
+      }
+
+
+    //   const submit = async event => {
+    //     event.preventDefault();
+    //     const response = await editUser(user)
+    //     console.log(response.data)
+    //     setMessage("Dodací údaje byly aktualizovány.")
+    //     setTimeout(() => {
+    //       setMessage("")
+    //     }, 1000)
+    //   }
    
     useEffect (() => {
         const getProductsAsync = async () => {
@@ -24,10 +49,33 @@ const Order = () =>{
     return (
         <div>
             <div>
-                <h1>Your Cart Items</h1>
+                <h1>Purchase</h1>
             </div>
                 <div>
+                    
                     <div>
+                        {/* <form className='formContainer' onSubmit={submit}> */}
+                        <form className='formContainer'>
+                            <h2 className="user-form-label">Delivery data</h2>
+
+                            <label className="user-form-label">Name</label>
+                            <input type="text" name="name" value={user.name} className="user-input" onChange={setInputField}/>
+
+                            <label className="user-form-label">E-mail</label>
+                            <input type="text" name="email" value={user.email} className="user-input" onChange={setInputField}/>
+                            
+                            <label className="user-form-label">City</label>
+                            <input type="text" name='city' value={user.city} className="user-input" onChange={setInputField}/>
+
+                            <label className="user-form-label">Address</label>
+                            <input type="text" name='address' value={user.address}className="user-input"onChange={setInputField}/>
+
+                            {/* <input type="submit" value="Uložit" className="user-input-button"/> */}
+                        </form>
+                    </div>
+                    <div className='formContainer'>
+                        <h2 className="user-form-label">Items in the cart</h2>
+
                         <div className='orderList'> 
                             {products.map((product) => {
                                 if(cartItems[product.id]){
@@ -35,8 +83,9 @@ const Order = () =>{
                                 }
                             })}
                         </div>
+                        <div className='totalPrice'>{totalPrice} -,E </div>  
                     </div>
-                    
+         
                 </div>
                     
         </div>
